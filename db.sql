f7ea5e5e098e11f068ace702bdac7910dcb84525
Marios Papachristou <papachristoumarios@gmail.com>
2018-05-23 10:42:57 +0300

    final version of db

--- db.sql
+++ db.sql
@@ -906,4 +906,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-23 10:42:19
+-- Dump completed on 2018-05-23 10:42:56
