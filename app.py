f19280f3b26f64469de9931497570fad9101dc8d
jim113 <xd4ever7@gmail.com>
2018-05-23 14:00:55 -0700

    Views in site

--- app.py
+++ app.py
@@ -151,6 +151,30 @@ def checkin():
 def reservation_error():
 	return render_template('reservation_error.html')
 
+@app.route('/Views', methods=['POST', 'GET'])
+def Views():
+	return render_template('Views.html')
+
+@app.route('/Views_result', methods=['POST', 'GET'])
+def Views_result():
+	global link_tables
+	error = False
+	search_results = []
+	if request.method == 'POST':
+		result = request.form
+		print(result.items())
+		for key, val in result.items():
+			if val == '': continue
+			print(val)
+			if(val=='capacity'):
+				query='SELECT * FROM eHOTELS.HotelRoomCapacityView'
+			if(val=='city'):
+				query='SELECT * FROM eHOTELS.HotelRoomLocationView'
+			search_results=exec_query(query)
+			print(search_results)
+	return render_template('Views_result.html',result = result, search_results = search_results, number_of_results = len(search_results), error=error)
+
+
 @app.route('/result_irs/<hotel_room_id>', methods=['POST', 'GET'])
 def result_irs(hotel_room_id):
 	if request.method == 'POST':
